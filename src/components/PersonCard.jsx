3e1581e14802f069810025ecbb9d7e6398face3f
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2024-02-26 16:40:14 -0300

    Button para aumentar la edad

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
     
     
 class PersonCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            position: this.props.age 
+        }
+    }
     render() {
         return (
             <div>
                 <h1>{this.props.firstName}, {this.props.lastName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.position}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.ageSwitch}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
+    }
+    ageSwitch = () =>{
+        this.setState({position: this.state.position + 1})
     }
 }
     
