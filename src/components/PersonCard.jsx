1d784a9b285f0cad8f8af5e5633383a043658c47
GerkeoDev <152994246+GerkeoDev@users.noreply.github.com>
2024-02-29 09:20:01 -0300

    Gran Inversion Functional Components

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
     
     
-class PersonCard extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            position: this.props.age 
-        }
-    }
-    render() {
-        return (
-            <div>
-                <h1>{this.props.firstName}, {this.props.lastName}</h1>
-                <p>Age: {this.state.position}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-                <button onClick={this.ageSwitch}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
-            </div>
-        )
-    }
-    ageSwitch = () =>{
-        this.setState({position: this.state.position + 1})
-    }
+// class PersonCard extends Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             position: this.props.age 
+//         }
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.props.firstName}, {this.props.lastName}</h1>
+//                 <p>Age: {this.state.position}</p>
+//                 <p>Hair Color: {this.props.hairColor}</p>
+//                 <button onClick={this.ageSwitch}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+//             </div>
+//         )
+//     }
+//     ageSwitch = () =>{
+//         this.setState({position: this.state.position + 1})
+//     }
+// }
+    
+// export default PersonCard;
+import React from "react";
+const PersonCard = (props) => {
+    return (
+        <div>
+            <h1>{props.firstName}, {props.lastName}</h1>
+            <p>Age: {props.age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+        </div>
+    )
 }
-    
-export default PersonCard;
+export default PersonCard
